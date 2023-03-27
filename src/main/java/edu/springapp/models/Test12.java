8a677427ff8ade66c30fcbd0035391e2418283a9
Temur Vegas <timanaimoff@gmail.com>
2023-03-27 19:37:34 +0500

    IoC

---
+++ src/main/java/edu/springapp/models/Test12.java
@@ -0,0 +1,13 @@
+package edu.springapp.models;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test12 {
+    public static void main(String[]args){
+        ClassPathXmlApplicationContext context=
+                new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        Pet pet=context.getBean("myPet",Pet.class);
+        pet.say();
+        context.close();
+    }
+}
