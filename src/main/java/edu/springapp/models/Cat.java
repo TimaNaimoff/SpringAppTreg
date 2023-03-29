689666201b59628de1d61557af8c7aad4609b899
Temur Vegas <timanaimoff@gmail.com>
2023-03-29 10:46:58 +0500

    Dependency Injection and Beans-Scope(Spring)

--- src/main/java/edu/springapp/models/Cat.java
+++ src/main/java/edu/springapp/models/Cat.java
@@ -2,10 +2,19 @@ package edu.springapp.models;
 
 
 public class Cat implements Pet{
+    private String name;
     public Cat(){
         System.out.println("Cat is created!");
     }
     public void say(){
         System.out.println("Meow meow!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
---
+++ src/main/java/edu/springapp/starters/Test4.java
@@ -0,0 +1,20 @@
+package edu.springapp.starters;
+
+import edu.springapp.models.Cat;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[]args){
+         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("classpath:applicationContextTwo.xml");
+         Cat cat=context.getBean("myPet", Cat.class);
+         cat.setName("Murris");
+         Cat cat2=context.getBean("myPet",Cat.class);
+         cat.setName("Turris");
+         System.out.println(cat.getName());
+         System.out.println(cat2.getName());
+         System.out.println(cat.equals(cat2));
+         System.out.println(cat+" : "+cat2);
+
+         context.close();
+    }
+}
